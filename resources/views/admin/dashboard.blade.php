26f409f572788eb4b894d5ab2b87f820eab1ded0
Admin <Butivchenko.Ruslan@gmail.com>
2023-03-03 12:29:14 +0200

    add scc

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     <main class="wrapper dashboard">
+        @include("layouts.nav-menu-home")
         <div class="dashboard-wrapper">
             @include("admin.sidebar")
             <div class="profile__wrapper">
