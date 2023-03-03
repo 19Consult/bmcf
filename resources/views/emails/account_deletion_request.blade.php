053c5e93f8b81822f83dcdde541c1c6deac77b22
Admin <Butivchenko.Ruslan@gmail.com>
2023-03-03 11:22:23 +0200

    update email admin

--- resources/views/emails/account_deletion_request.blade.php
+++ resources/views/emails/account_deletion_request.blade.php
@@ -1,1 +1,12 @@
-{{$text}}
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>{{$title}}</title>
+    </head>
+    <body>
+        <div class="container">
+            {{$text}}
+        </div>
+    </body>
+</html>
