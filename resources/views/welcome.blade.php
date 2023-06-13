18004d9c5eb3e80f60539cdda9f3c0dd49d0e39d
Ruslan <ruslan.butivchenko.dev@gmail.com>
2023-06-13 14:53:09 +0300

    update_13-06

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,11 @@
 
 @section('content')
 
+@php
+    $role_token = app("roleIdMD5_register");
+    $role_md5 = $role_token[3];
+@endphp
+
 <main class="wrapper home-page">
     <div class="container">
         <div class="home-page__top">
@@ -9,20 +14,57 @@
         </div>
         <div class="home-page__middle">
             <div class="col left">
-                <div class="home-page__title-top">Looking FOR a</div>
-                <h2 class="home-page__title">COFOUNDER?</h2><a class="btn btn--border" href="{{ route('sign-up-founder') }}">START HERE</a>
-                <div class="home-page__descr-wrapper">
-                    <div class="home-page__descr-title">time to move forward-fast</div>
-                    <div class="home-page__descr">Being a founder can be a lonely business and hard work is seldom enough to guarantee success. BeMyCoFounder is a FREE platform to help match you with the experts your project needs.</div>
+                <div class="home-page__middle-left">
+                    <div class="home-page__title-top">Looking FOR</div>
+                    <h2 class="home-page__title">COFOUNDERS?</h2>
+                    <div class="home-page__descr-wrapper">
+                        <div class="home-page__descr-title">time to move forward-fast</div>
+                        <div class="home-page__descr">Being a founder can be a lonely business and hard work is seldom enough to guarantee success. BeMyCoFounder is a FREE platform to help match you with the experts your project needs.</div>
+                    </div>
+                </div>
+
+                <div class="home-page__middle-right">
+                    <div class="home-page__title-top">WANT TO BE A</div>
+                    <h2 class="home-page__title">COFOUNDER?</h2>
+                    <div class="home-page__descr-wrapper">
+                        <div class="home-page__descr-title">True value of you</div>
+                        <div class="home-page__descr">Realise the full value of your skills, market knowledge and experience with BeMyCoFounder. Free sign up will connect you with unique opportunities to team up with innovators and start-ups to do what you love and on your terms.</div>
+                    </div>
+                    <a class="btn btn--border" href="{{ route('sign-up-founder') }}">START HERE</a>
                 </div>
             </div>
             <div class="col right">
-                <div class="home-page__title-top">I WANT TO BE A</div>
-                <h2 class="home-page__title">COFOUNDER!</h2><a class="btn btn--border" href="{{ route('sign-up-co-founder') }}">START HERE</a>
-                <div class="home-page__descr-wrapper">
-                    <div class="home-page__descr-title">True value of you</div>
-                    <div class="home-page__descr">Realise the full value of your skills, market knowledge and experience with BeMyCoFounder. Free sign up will connect you with unique opportunities to team up with innovators and start-ups to do what you love and on your terms.</div>
-                </div>
+                <div class="right-title">Join BeMyCoFounder.com for FREE <br> and Find Your A-Team!</div>
+                <div class="right-descr">Add your project today and connect with those fit your ambition.</div>
+                <form class="type-1" method="POST" action="{{ route('sign-up-co-founder') }}">
+                    @csrf
+                    <div class="form_input_wrap">
+                        @error('email')
+                        <span class="invalid-feedback error-message" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                        <input type="mail" id="email" placeholder="Your Email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                        <label for="email">Email</label>
+                    </div>
+                    <div class="form_input_wrap password">
+                        @error('password')
+                        <span class="invalid-feedback error-message" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                        <input type="password" id="password" placeholder="Your Password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                        <label for="password">Password</label>
+                        <input class="show-password" type="checkbox">
+                    </div>
+                    <div class="form_input_wrap password">
+                        <input type="password" id="password" placeholder="Your Password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                        <label for="password">Repeat password</label>
+                        <input class="show-password" type="checkbox">
+                    </div>
+                    <input type="hidden" name="roidle" value="{{$role_md5}}">
+                    <button type="submit" class="btn btn--solid btn--arrow">Register</button>
+                </form>
             </div>
         </div>
         <div class="home-page__bottom">
